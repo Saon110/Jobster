f0749d406ae893b64cdaa1cf2586c7da56634829
Sijon Chisty Saon <2105110@ugrad.cse.buet.ac.bd>
2024-02-24 12:23:46 +0600

    s

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,11 +8,17 @@ import UpdatePage from './routes/user/UpdatePage';
 import Login from './routes/Login';
 
 // import CompanyList from './components/user/CompanyList';
-import Jobs from './routes/company/Jobs';
+import jobs from './routes/company/Jobs';
 import Employees from './routes/company/Employees';
 import CompanyHome from './routes/company/Home';
 import CompanyAddJob from './routes/company/AddJob';
-
+import Companies from './routes/user/Companies';
+import Jobs from './routes/user/Jobs';
+import Apply from './routes/user/Apply';
+import Myprofile from './routes/user/Myprofile';
+import UpdateProfile from './components/user/UpdateProfile';
+import SearchBar from './components/user/SearchBar';
+import AddCompany from './components/user/AddCompany';
 const App = () => {
   return (
 
@@ -25,8 +31,14 @@ const App = () => {
                 <Route path="/company/:companyId" element={<CompanyDetails/>} />
                 <Route path="/company/:companyId/update" element={<UpdatePage/>} />
                 <Route path ="/User/home" element = {<Home/> } />
+                <Route path ="/User/Companies" element = {<Companies/> } />
+                <Route path ="/User/Jobs" element = {<Jobs/> } />
+                <Route path ="/User/Apply" element = {<Apply/> } />
+                <Route path = "/User/Myprofile"element = {<Myprofile/>} />
+                <Route path = "/User/Myprofile/Update" element = {<UpdateProfile/> } />
+                <Route path ="/dummy" element = {<AddCompany/>} />
 
-                <Route path ="/Employer/:id/jobs" element = {<Jobs/>} />
+                <Route path ="/Employer/:id/jobs" element = {<jobs/>} />
                 <Route path ="/Employer/:id/employees" element = {<Employees/>} />
                 <Route path ="/Employer/:id" element = {<CompanyHome/>} />
                 <Route path ="/Employer/:id/addjob" element = {<CompanyAddJob/>} />
